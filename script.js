4ae513a78d1d6f095446561aee8bd222cd156e8e
Em <contactemilyhuang@gmail.com>
2021-11-18 05:56:43 -0800

    Add HTML and JS

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("To start a game, write and enter startGame();");
+
+function computerPlay(){
+    return Math.floor(Math.random() * 3);
+}
+
+function userPlay(){
+    return window.prompt("Enter rock, paper, or scissors below.","rock");
+}
+
+function startGame(){
+    userSelection = userPlay();
+    convertUserInput(userSelection);
+    userSelection = convertUserInput(userSelection);
+    computerPlay();
+    computerSelection = computerPlay();
+    convertComputerInput(computerSelection);
+    computerSelection = convertComputerInput(computerSelection);
+    compareSelection(userSelection,computerSelection);
+}
+
+function convertComputerInput(computerSelection){
+    if (computerSelection == 0){
+        return computerSelection = "rock";
+    }
+    else if (computerSelection == 1){
+        return computerSelection = "paper";
+    }
+    else if (computerSelection == 2){
+        return computerSelection = "scissors";
+    }
+}
+
+function convertUserInput(userSelection){
+    return userSelection.toLowerCase();
+}
+
+function compareSelection(userSelection,computerSelection){
+    if(userSelection == computerSelection){
+        return console.log(`You played ${userSelection}. The computer played ${computerSelection}. It's a draw!`);
+    }
+    else if(userSelection == "rock" && computerSelection == "scissors" || userSelection == "paper" && computerSelection == "rock" || userSelection == "scissors" && computerSelection == "paper"){
+        return console.log(`You played ${userSelection}. The computer played ${computerSelection}. You win!`);
+    }
+    else if (userSelection == "rock" && computerSelection == "paper" || userSelection == "paper" && computerSelection == "scissors" || userSelection == "scissors" && computerSelection == "rock"){
+        return console.log(`You played ${userSelection}. The computer played ${computerSelection}. You lose!`);
+    }
+}
